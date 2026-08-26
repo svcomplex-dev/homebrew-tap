e63dc6f297067e032c1d7ee97951b86535919baf
svcomplex-dev <code@svcomplex.ai>
2026-08-26 00:54:31 +0000

    brew: update svw for latest

--- Formula/svw-latest.rb
+++ Formula/svw-latest.rb
@@ -3,9 +3,9 @@ class SvwLatest < Formula
   homepage "https://svw.run"
   url "https://github.com/svcomplex-dev/svw/releases/download/latest/svw-latest-macos-arm64.tar.gz"
   version "0.1.0"
-  sha256 "25b06ab319740a424a4ca4fd407aa327cee915f3771fec43713274e133273071"
+  sha256 "89ebeeee336dfc10ec036463108a55547500948bb60a3fac3532cf3b3e4fb0a1"
   license "MIT"
-  revision 17
+  revision 18
 
   depends_on arch: :arm64
   depends_on macos: :big_sur
