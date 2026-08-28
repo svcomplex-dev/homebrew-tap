98b404fccbf0f32dc14dc13d8dd145ead28afdd6
svcomplex-dev <code@svcomplex.ai>
2026-08-28 13:08:41 +0000

    brew: update svw for latest

--- Formula/svw-latest.rb
+++ Formula/svw-latest.rb
@@ -2,10 +2,9 @@ class SvwLatest < Formula
   desc "Terminal waveform viewer for hardware design workflows"
   homepage "https://svw.run"
   url "https://github.com/svcomplex-dev/svw/releases/download/latest/svw-latest-macos-arm64.tar.gz"
-  version "0.1.0"
-  sha256 "581994b62d64067cdff86d28ecc2bf8daa1c498fdb7d0c27b7f7b0b743b6c538"
+  version "0.1.1"
+  sha256 "0688bc4998a1efa771a53c834b1289377495d6f57ebce24623e68e82fa6a5c20"
   license "MIT"
-  revision 27
 
   depends_on arch: :arm64
   depends_on macos: :big_sur
